f50925b1fc51b7ab55b60e3063fd22bcd741c6a4
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2022-02-04 16:42:32 +0900

    jQuery - 기본구문
    $('선택자').함수(function)(){ 앤터};

---
+++ jQuery-기본구문/script/custom.js
@@ -0,0 +1,10 @@
+$('p').css({'display':'none'});
+$('.show-btn').click(function(){
+  $('p').css({'display':'block'});
+});
+$('.hide-btn').click(function(){
+  $('p').css({'display':'none'});
+});
+
+$('선택자').click(function(){
+})
